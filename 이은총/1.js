a3144d27b948d4c70e01d637af874249bd4cbb32
EunChong Lee <104499306+eunchong2lee@users.noreply.github.com>
2022-05-13 12:09:27 +0800

    Update 1.js

--- 이은총/1.js
+++ 이은총/1.js
@@ -2,6 +2,10 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-    console.log(a);
-    console.log(b);
+    for(i=0;i<b;i++){
+        console.log('*'.repeat(a))
+    }    
 });
+
+
+// https://programmers.co.kr/learn/courses/30/lessons/12969
