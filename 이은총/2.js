438c6aad39cdaa9d9e9abf7cb511f6bf97318e00
EunChong Lee <104499306+eunchong2lee@users.noreply.github.com>
2022-05-13 12:09:57 +0800

    Update 2.js

---
+++ 이은총/2.js
@@ -0,0 +1,8 @@
+function solution(num) {
+    var answer = '';
+    if (num%2==0) answer = 'Even'    
+    else answer = 'Odd'
+    return answer;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12937
