5f399897b833063035b220af13c5edcc442a96e3
Hayeon Kim <85288036+HayeonKimm@users.noreply.github.com>
2022-05-13 13:03:35 +0900

    Create 2.js

---
+++ 김하연/2.js
@@ -0,0 +1,12 @@
+function solution(num) {
+    var answer = '';
+    
+    if(num%2===0){
+        var answer = "Even";
+    } else
+        var answer = "Odd";
+    
+    
+    
+    return answer;
+}
